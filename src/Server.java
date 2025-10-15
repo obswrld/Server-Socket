551e20389cc9f19672a9b82470add1f25c237d51
Republic Oba <republicoba1@gmail.com>
2025-10-15 14:54:10 +0100

    server socket

---
+++ src/Server.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = null;
+        InputStreamReader inputStreamReader = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        ServerSocket serverSocket = null;
+        serverSocket = new ServerSocket(1234);
+
+        while (true) {
+            try {
+
+                socket = serverSocket.accept();
+                inputStreamReader = new InputStreamReader(socket.getInputStream());
+                outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+                bufferedReader = new BufferedReader(inputStreamReader);
+                bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+                while(true){
+
+                    String messageFromClient = bufferedReader.readLine();
+                    System.out.println("Helen: "+messageFromClient);
+                    bufferedWriter.write("Message Received ");
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+
+                    if(messageFromClient.equalsIgnoreCase("exit"))
+                        break;
+                }
+                socket.close();
+                inputStreamReader.close();
+                outputStreamWriter.close();
+                bufferedReader.close();
+                bufferedWriter.close();
+                
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
